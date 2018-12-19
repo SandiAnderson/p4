05041c3a884230858cc646d413f4afaa2c49be6a
Sandra Anderson <anderson.sandi@gmail.com>
2018-12-18 17:00:25 -0800

    update formatting

--- resources/views/layouts/challengeheader.blade.php
+++ resources/views/layouts/challengeheader.blade.php
@@ -1,4 +1,5 @@
-    <div class='nav2'>
-        @foreach(config('app.challenge') as $nav2)
-            <a href="{{$nav2['link']}}">{{$nav2['label']}}</a>
+<div class='nav2'>
+    @foreach(config('app.challenge') as $nav2)
+        <a href="{{$nav2['link']}}">{{$nav2['label']}}</a>
     @endforeach
+</div>
